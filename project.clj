a687cd5bcd0d9943af04a00dd01cb122cc2a1731
leonid <lkonontsov@hotmail.com>
2020-01-08 13:34:59 +0300

    add: dependecies

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "GPL-3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.blancas/kern "1.1.0"]]
   :main ^:skip-aot tokiilo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
